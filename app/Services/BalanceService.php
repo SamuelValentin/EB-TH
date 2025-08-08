db06eed424c63028451ae79b8d189c16b8f7ba18
SamuelValentin <samuellv89@gmail.com>
2025-08-08 09:47:34 -0300

    Balance class logic implemented

--- app/Services/BalanceService.php
+++ app/Services/BalanceService.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 class BalanceService
 {
     public function getBalance(Request $request, array &$accounts){
+        if (!isset($accounts[$request->input('account_id')])) {
+            throw new \Exception("Account not found");
+        }
+        return $accounts[$request->input('account_id')]['balance'];
     }
 }
