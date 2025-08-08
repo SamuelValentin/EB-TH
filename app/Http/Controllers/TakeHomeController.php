e75d02114824be7b849c09fa218935b5e5220546
SamuelValentin <samuellv89@gmail.com>
2025-08-07 21:21:21 -0300

    Route init

---
+++ app/Http/Controllers/TakeHomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TakeHomeController extends Controller
+{
+    public function reset(){
+
+    }
+
+    public function balance(Request $request){
+
+    }
+
+    public function event(Request $request){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\TakeHomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/reset', [TakeHomeController::class, 'reset'])->name('reset');
+Route::get('/balance', [TakeHomeController::class, 'balance'])->name('balance');
+Route::post('/event', [TakeHomeController::class, 'event'])->name('event');
